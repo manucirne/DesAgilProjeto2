fc001a42e121aeb60a353e554936c38d1859f80f
manucirne <manucirne@gmail.com>
2018-04-13 15:52:13 -0300

    entrega 3 funcionando, mas com código um pouco sujo

--- src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/GateType.java
+++ src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/view/GateType.java
@@ -1,17 +1,25 @@
 package br.pro.hashi.ensino.desagil.rafaelogic.view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JCheckBox;
 
+import br.pro.hashi.ensino.desagil.rafaelogic.view.SimplePanel;
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Gate;
 import br.pro.hashi.ensino.desagil.rafaelogic.model.Source;
 
-public class GateType extends JPanel implements ItemListener{
+public class GateType extends SimplePanel implements ItemListener, MouseListener{
 
 	//Linha que ainda não entendemos
 	private static final long serialVersionUID = 1L;
@@ -25,9 +33,18 @@ public class GateType extends JPanel implements ItemListener{
 	//dois sources - acho que deveríamos usar apenas uma e definir com o pinIndex
 	//como ela será ligada. Todas as tentativas até agora falharam
 	
+	// Estes nomes aqui sÃ£o auto-explicativos, nÃ£o?
+	private Color color;
+	private Image image;
 
 
 	public GateType(Gate gate) {
+		
+		super(245, 300);
+		
+		// Inicializamos esse atributo com o preto.
+		color = Color.BLACK;
+		
 		//define qual porta está sendo usada
 		this.gate = gate;
 		//vetor de checkboxes de entrada sendo criado
@@ -43,18 +60,26 @@ public class GateType extends JPanel implements ItemListener{
 		JLabel saida = new JLabel("Saída");
 
 		// Leyout - um abaixo do outro
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		//Adiciona a label de entrada à tela
-		add(entrada);
+		add(entrada,20,20,50,20);
 		//for que coloca, na tela, os checkboxes de entrada e os sources de acordo
 		// com o size da porta selecionada
 		for(int i = 0; i < gate.size(); i++){
 			JCheckBox checkboxG = new JCheckBox();
 			checkboxG.setSelected(false);
-			add(checkboxG);
-			//os checkboxes de entrada devem ser "escutados"
-			checkboxG.addItemListener(this);
+			if(gate.size()==1){
+				add(checkboxG,20,130,20,20);
+				//os checkboxes de entrada devem ser "escutados"
+				checkboxG.addItemListener(this);
+			}
+			else{
+				add(checkboxG,20,i*84+88,20,20);
+				//os checkboxes de entrada devem ser "escutados"
+				checkboxG.addItemListener(this);
+			}
+			
 			//vetor de checkboxes recebendo um checkbox de entrada
 			checkbox[i] = checkboxG;
 			//vetor de sources recebendo um source
@@ -67,9 +92,9 @@ public class GateType extends JPanel implements ItemListener{
 		}
 		
 		//label de saída adicionado a tela
-		add(saida);
+		add(saida,170,20,50,20);
 		//checkbox de saída adicionado a tela
-		add(checkboxS);
+		//add(checkboxS,180,130,20,20);
 		
 		
 	
@@ -79,6 +104,19 @@ public class GateType extends JPanel implements ItemListener{
 
 		//Para que a porta correspoda ao estado dos pinos desde o início
 		update();
+		
+
+		// Usamos isso no Projeto 1, vocÃªs lembram?
+		String path = "/" + gate.toString() + ".png";
+		URL url = getClass().getResource(path);
+		image = new ImageIcon(url).getImage();
+		
+		// Toda componente Swing possui este mÃ©todo, que
+		// permite adicionar objetos que reagem a eventos
+		// de mouse que acontecem nela. Ou seja, ao passar
+		// this como parÃ¢metro, estamos pedindo para a
+		// componente reagir aos prÃ³prios eventos de mouse.
+		addMouseListener(this);
 	}
 	
 	@Override
@@ -122,12 +160,67 @@ public class GateType extends JPanel implements ItemListener{
 				
 		}
 
-		
-		//Diz para o checkbox de saída o que deve ser feito (true ou false)
-		//da leitura do gate com os pinos de entrada
-		checkboxS.setSelected(gate.read());
+	
 	
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent event) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+
+		// NÃ£o podemos esquecer desta linha, pois nÃ£o somos os
+		// Ãºnicos responsÃ¡veis por desenhar o painel, como era
+		// o caso no Projeto 1. Agora Ã© preciso desenhar tambÃ©m
+		// componentes internas, e isso Ã© feito pela superclasse.
+		super.paintComponent(g);
+
+		// Desenha a imagem passando posiÃ§Ã£o e tamanho.
+		// O Ãºltimo parÃ¢metro pode ser sempre null.
+		g.drawImage(image, 10, 40, 200, 200, null);
+		
+		if(gate.read()== true){
+			g.setColor(Color.RED);
+			
+		}
+		else{
+			g.setColor(Color.BLACK);
+		}
+		g.fillOval(180,130,20,20);
+		repaint();
+		
+		// Evita bugs visuais em alguns sistemas operacionais.
+		getToolkit().sync();
+		
+		update();
+    }
 
 	
 
